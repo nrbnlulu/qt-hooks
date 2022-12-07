03769594028f39f55e0067fd3c5940cd553f9334
Nir <88795475+nrbnlulu@users.noreply.github.com>
2022-12-07 23:05:04 +0200

    fc

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
 setup(
-
+    install_requires=[
+        'PySide6==6.4.1',
+    ],
+    tests_require=['pytest'],
 )
---
+++ tests/test_formatter/test_formatter.py
@@ -0,0 +1,24 @@
+import subprocess
+import sys
+import pytest
+from qmlformatter import main
+from pathlib import Path
+
+def test_formats():
+    sample_qml = Path(__file__).parent / 'sample.qml'
+    assert sample_qml.exists()
+    with open(sample_qml, 'r') as f:
+        before = f.read()
+    with open(sample_qml, 'w') as f:
+        messed = before.replace('\n', '\n \n')
+        f.write(messed)
+
+    p = subprocess.Popen(
+        ['qmlformatter', str(sample_qml.resolve())]
+    )
+    p.wait(2000)
+    assert p.returncode == 0
+    with open(sample_qml, 'r') as f:
+        after = f.read()
+
+    assert messed != after
